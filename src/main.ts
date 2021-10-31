16f3b28eb7fb493af444845b68655105bab4a9ac
Reinaldo Nascimento <reinaldo.nascimento@queimadiaria.com.br>
2021-10-31 15:56:02 -0300

    feat: rebase last branch

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 const PRIMEIRA_ALTERACAO = 'Primeira alteração revisada';
-
 Logger.log(PRIMEIRA_ALTERACAO);
 
 const UPDATE_DIRECT = 'Foi alterado diretamente';
-
 Logger.log(UPDATE_DIRECT);
-const PRIMEIRA_ALTERACAO = 'Primeira alteração';
-const SEGUNDA_ALTERACAO = 'Segunda alteração';
 
-Logger.log(PRIMEIRA_ALTERACAO);
+const SEGUNDA_ALTERACAO = 'Segunda alteração';
 Logger.log(SEGUNDA_ALTERACAO);
 
 async function bootstrap() {
