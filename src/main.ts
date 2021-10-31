e8cc5f28f5446e47893b19760749f32038f3fca9
Reinaldo Nascimento <80479416+reinaldoqd@users.noreply.github.com>
2021-10-31 15:56:49 -0300

    Feature/alt 002 (#2)
    * feat: primeira alteracao
    
    * feat: segunda alteracao
    
    * feat: rebase last branch

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 const PRIMEIRA_ALTERACAO = 'Primeira alteração revisada';
-
 Logger.log(PRIMEIRA_ALTERACAO);
 
 const UPDATE_DIRECT = 'Foi alterado diretamente';
-
 Logger.log(UPDATE_DIRECT);
+
+const SEGUNDA_ALTERACAO = 'Segunda alteração';
+Logger.log(SEGUNDA_ALTERACAO);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
