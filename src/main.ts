ab3bd000908aae09b44cc922f7a73aa2be38d9d7
Reinaldo Nascimento <reinaldo.nascimento@queimadiaria.com.br>
2021-10-31 15:28:21 -0300

    feat: segunda alteracao

--- src/main.ts
+++ src/main.ts
@@ -10,8 +10,10 @@ const UPDATE_DIRECT = 'Foi alterado diretamente';
 
 Logger.log(UPDATE_DIRECT);
 const PRIMEIRA_ALTERACAO = 'Primeira alteração';
+const SEGUNDA_ALTERACAO = 'Segunda alteração';
 
 Logger.log(PRIMEIRA_ALTERACAO);
+Logger.log(SEGUNDA_ALTERACAO);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
