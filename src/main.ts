5fb3de17cd7da0594f5f4842dfd84795f40d95c2
Reinaldo Nascimento <reinaldo.nascimento@queimadiaria.com.br>
2021-10-31 15:27:10 -0300

    feat: primeira alteracao

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+
+const PRIMEIRA_ALTERACAO = 'Primeira alteração';
+
+Logger.log(PRIMEIRA_ALTERACAO);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
