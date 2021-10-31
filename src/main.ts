f30757bbfc462c63b60a4126f86481b3dfc42b11
Reinaldo Nascimento <80479416+reinaldoqd@users.noreply.github.com>
2021-10-31 15:59:23 -0300

    feat: ultimo branch (#3)

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,9 @@ Logger.log(UPDATE_DIRECT);
 const SEGUNDA_ALTERACAO = 'Segunda alteração';
 Logger.log(SEGUNDA_ALTERACAO);
 
+const TERCEIRA_ALTERACAO = 'Terceira alteração';
+Logger.log(TERCEIRA_ALTERACAO);
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
