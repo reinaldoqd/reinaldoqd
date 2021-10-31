95bd07c65c84e7a78e636d38cabd0d83c4df23e1
Reinaldo Nascimento <80479416+reinaldoqd@users.noreply.github.com>
2021-10-31 15:38:16 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+
+const UPDATE_DIRECT = 'Foi alterado diretamente';
+Logger.log(UPDATE_DIRECT).
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
